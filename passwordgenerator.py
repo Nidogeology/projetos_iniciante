ee7377a2cb907c255481dcf9b8c10ba33a5c75c9
Fernando Pacheco <ferodrigueslive.it>
2020-03-17 12:32:20 -0300

    Usando secrets

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,9 +1,5 @@
-import random
+import secrets
 import string
-
-def generator(i):
-    characteres = string.ascii_letters + string.digits
-    return ''.join(random.choice(characteres) for i in range(i))
 
 print("Vamos criar três senhas aleatórias. Quantos caracteres você quer que elas tenham? (Entre 6 e 20)")
 i = int(input())
@@ -11,9 +7,7 @@ while i < 6 or i >20:
     print("A senha deve ter entre 6 e 20 caracteres. Quantos caracteres você quer?")
     i = int(input())
 else:
-     print("Gerando uma senha:")
-     print("Primeira senha é: ", generator(i))
-     print("Segunda senha é: ", generator(i))
-     print("Terceira senha é: ", generator(i))
-
-
+    print("Gerando uma senha:")
+    print("Primeira senha é: ", secrets.token_urlsafe(i))
+    print("Segunda senha é: ", secrets.token_urlsafe(i))
+    print("Terceira senha é: ", secrets.token_urlsafe(i))

c749006d5839b95f4c1da00e824ce12b3d25578b
Fernando Pacheco <ferodrigueslive.it>
2020-03-17 21:03:47 -0300

    Corrigindo quantidade

--- dailyroutine.py
+++ dailyroutine.py
@@ -3,7 +3,7 @@ from datetime import date
 
 def randomroutine():
     exercises1 = ["100 Flexão de braço", "100 Panturrilha", "400 Polichinelo", "200 Agachamento", "200 Abdominal supra"]
-    exercises2 = ["200 Abdominal Infra", "400 Joelho alto", "100 Climber", "200 Passada", "100 Salto"]
+    exercises2 = ["200 Abdominal Infra", "200 Joelho alto", "100 Climber", "200 Passada", "100 Salto"]
     a0 = int(len(exercises1))
     b0 = int(len(exercises2))
     a = exercises1
